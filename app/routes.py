ab584ff958ac0ffb108a09a566d3b503a8c71ff7
sssfilho <silvanssfilho@gmail.com>
2023-08-21 17:25:01 -0300

    cript
    cripto e banco de dados

--- app/routes.py
+++ app/routes.py
@@ -1,64 +1,43 @@
-from app import app, db
-from flask import render_template, url_for, flash, redirect, request, session
+from app import app , db, bcrypt
+from flask import render_template, url_for, request, flash, session, redirect
 from app.forms import Contato, Cadastro
 from app.models import ContatoModels, CadastroModel
-import time 
+from flask_bcrypt import check_password_hash
+
+import time
+
 
 @app.route('/')
-@app.route('/index')
-
 def index():
-    return render_template('index.html', title = 'Início')
+    return render_template('index.html',titulo = 'Página inicial')
 
-@app.route('/sobre')
-def sobre():
-    return render_template('sobre.html', title = 'Sobre')
-
-@app.route('/projeto', methods=['GET','POST'])
-def projeto():
-    return render_template('projeto.html', title='projeto')
+@app.route('/contatos', methods=['POST', 'GET'])
+def contatos():
     
-
-@app.route('/contato', methods=['GET', 'POST'])
-def contato():
-    dados_formulario = None
     formulario = Contato()
+    print('Acessou a rota contatos!')
     if formulario.validate_on_submit():
-        flash('SEU FORMULARIO FOI ENVIADO COM SUCESSO !!!')
+        flash('Seu cadastro foi enviado com sucesso!')
         nome = formulario.nome.data
         email = formulario.email.data
-        telefone = formulario.telefone.data
-        conteudo = formulario.conteudo.data
-        novo_contato = ContatoModels(nome=nome, email=email, telefone=telefone, conteudo=conteudo)
+        mensagem = formulario.mensagem.data
+        
+        novo_contato = ContatoModels(nome=nome,email=email,mensagem=mensagem)
         db.session.add(novo_contato)
         db.session.commit()
+    return render_template('contatos.html', titulo = 'Contatos',formulario = formulario)
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html', titulo = 'Sobre')
 
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html', titulo = 'Projetos' )
 
-        
-    return render_template('contato.html', title='Contato', formulario = formulario, dados_formulario = dados_formulario)
-        
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', tituto = 'Blog')
 
-@app.route('/teste')    
-def teste():
-    return render_template('teste.html')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        senha = request.form.get('senha')
-        user = CadastroModel.query.filter_by(email = email, senha = senha).first()
-        
-        if user and user.senha == senha:
-            session['user_id'] = user.id
-            flash('Seja bem vindo')
-            time.sleep(3)
-            return redirect(url_for('index'))
-        else:
-            flash('Senha ou email incorreto!')
-
-    return render_template('login.html')
-        
 @app.route('/cadastro', methods=['POST', 'GET'])
 def cadastro():
     print('Acessou a rota de cadastro')
@@ -69,14 +48,55 @@ def cadastro():
             sobrenome = cadastro.sobrenome.data
             email = cadastro.email.data
             senha = cadastro.senha.data
-            novo_cadastro = CadastroModel(nome=nome, sobrenome=sobrenome, email=email, senha=senha)
+            hash_senha = bcrypt.generate_password_hash(senha).decode('utf-8')
+            novo_cadastro = CadastroModel(nome=nome, sobrenome=sobrenome, email=email, senha=hash_senha)
             db.session.add(novo_cadastro)
             db.session.commit()
             flash('Seu cadastro foi realizado com sucesso!')
         except Exception as e:
-            flash('Ocorreu um erro ao cadastrar! Entre em contato como suporte: adm@admin.com')
+            flash('Ocorreu um erro ao cadastrar! Entre em contato com o suporte: adm@admin.com')
             print(str(e))
     return render_template('cadastro.html', titulo='Cadastro', cadastro=cadastro)
 
-   
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        user = CadastroModel.query.filter_by(email=email).first()
+        
+        if user and check_password_hash(user.senha, senha):
+            session['email'] = user.email  
+            session['nome'] = user.nome
+            session['sobrenome'] = user.sobrenome
+            session['senha'] = user.senha
+            flash('Seja bem vindo')
+            return redirect(url_for('index')) 
+        else:
+            flash('Senha ou e-mail incorreto!')
+    return render_template('login.html', titulo='Login')
+@app.route('/sair')
+def sair():
+    session.pop('email', None)
+    session.pop('nome', None)
+    return redirect(url_for('login'))
+
+@app.route('/editar', methods=['POST', 'GET'])
+def editar():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    usuario = CadastroModel.query.filter_by(email = session['email']).first()
+    if request.method == 'POST':
+        usuario.nome = request.form.get('nome')
+        usuario.sobrenome = request.form.get('sobrenome')
+        usuario.email = request.form.get('email')
+        senha = request.form.get('senha')
+        usuario.senha = bcrypt.generate_password_hash(senha).decode('utf-8')
+        db.session.commit()
+        session['email'] = usuario.email  
+        session['nome'] = usuario.nome
+        session['sobrenome'] = usuario.sobrenome
+        session['senha'] = usuario.senha
+        flash('Seus dados foram atualizados com sucesso!')
+    return render_template('editar.html', titulo= 'Editar', usuario = usuario)
